68a8500dd39a10043da965a97f5cfb85d49d5dae
Jeshua Morrissey <jeshuamorrissey@gmail.com>
2017-01-04 21:26:22 +1100

    you can now log in using facebook

--- smgm/__init__.py
+++ smgm/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from flask.ext.login import LoginManager
+
 app = Flask(__name__)
+app.secret_key = 'super secret'
 
 import smgm.views
+
+# OAuth login scripts.
+import smgm.login
--- smgm/views.py
+++ smgm/views.py
@@ -1,5 +1,11 @@
 from smgm import app
 
+from flask import redirect, url_for, render_template
+
 @app.route('/')
 def index():
-  return 'Hello, World!'
+  return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
