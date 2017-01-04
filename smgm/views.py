30e6a52607f5f0e5e75c52326c7965ed1cb0b225
Jeshua Morrissey <jeshuamorrissey@gmail.com>
2017-01-04 17:33:46 +1100

    fixed

--- smgm/views.py
+++ smgm/views.py
@@ -1,17 +1,5 @@
 from smgm import app
 
-from flask_oauth import OAuth
-
-facebook = oauth.remote_app('facebook',
-  base_url='https://graph.facebook.com/',
-  request_token_url=None,
-  access_token_url='/oauth/access_token',
-  authorize_url='https://www.facebook.com/dialog/oauth',
-  consumer_key=FACEBOOK_APP_ID,
-  consumer_secret=FACEBOOK_APP_SECRET,
-  request_token_params={'scope': 'email'}
-)
-
 @app.route('/')
 def index():
   return 'Hello, World!'
