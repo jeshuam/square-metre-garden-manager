22eb31a0831efa981e85a6e07228a931a4d016e3
Jeshua Morrissey <jeshuamorrissey@gmail.com>
2017-01-05 14:25:52 +1100

    added comment

--- smgm/__init__.py
+++ smgm/__init__.py
@@ -3,6 +3,9 @@ from flask_stormpath import StormpathManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret'
+
+# TODO(jeshua): This stuff should not be here. Like, it really shouldn't. If
+# this ever becomes a real thing, please move these somewhere more secure.
 app.config['STORMPATH_API_KEY_ID'] = '646KV4LB30OGVAMHVEMFGK0N0'
 app.config['STORMPATH_API_KEY_SECRET'] = 'JSucwgxmRec03OV7D91+fLVg700JGAR8PvljlK2k3t4'
 app.config['STORMPATH_APPLICATION'] = 'Square Metre Garden Manager'
