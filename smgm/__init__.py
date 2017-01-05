286ad158d168e82712e96491a8fdaad2468e7a3d
Jeshua Morrissey <jeshuamorrissey@gmail.com>
2017-01-05 14:25:00 +1100

    using stormpath (which is awesome)

--- smgm/__init__.py
+++ smgm/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from flask.ext.login import LoginManager
+from flask_stormpath import StormpathManager
 
 app = Flask(__name__)
-app.secret_key = 'super secret'
+app.config['SECRET_KEY'] = 'super secret'
+app.config['STORMPATH_API_KEY_ID'] = '646KV4LB30OGVAMHVEMFGK0N0'
+app.config['STORMPATH_API_KEY_SECRET'] = 'JSucwgxmRec03OV7D91+fLVg700JGAR8PvljlK2k3t4'
+app.config['STORMPATH_APPLICATION'] = 'Square Metre Garden Manager'
+
+stormpath_manager = StormpathManager(app)
 
 import smgm.views
-
-# OAuth login scripts.
-import smgm.login
--- smgm/views.py
+++ smgm/views.py
@@ -1,11 +1,7 @@
 from smgm import app
 
-from flask import redirect, url_for, render_template
+from flask import render_template
 
 @app.route('/')
 def index():
-  return redirect(url_for('login'))
-
-@app.route('/login')
-def login():
-  return render_template('login.html')
+  return render_template('index.html')
